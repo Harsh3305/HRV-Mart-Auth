785ec9daf1ea053d1918cfa65f61855f67c3b068
Harsh3305 <harshverma3305@gmail.com>
2022-12-14 22:37:48 +0530

    Allow service to get request body in json formate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const { json } = require("express")
 const authRoute = require("./routes/auth");
 require('dotenv').config();
 
 const app = express();
-
+app.use(json())
 app.use("", authRoute);
 
 app.listen(process.env.PORT, (error) => {
 	if (error) {
 		console.error(error);
-}
-    	console.log("Backen is started");
+	}
+	console.log("Backen is started");
 });
