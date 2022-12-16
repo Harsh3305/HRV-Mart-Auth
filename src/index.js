de07eb186d3b5a9726ea6bdf002012dc943b1469
Harsh3305 <harshverma3305@gmail.com>
2022-12-16 13:53:33 +0530

    Added cors in app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const authRoute = require("./routes/auth");
 const homeRoute = require("./routes/home");
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors)
 app.use("", authRoute);
 app.use("", homeRoute);
 
 app.listen(process.env.PORT, (error) => {
 	if (error) {
 		console.error(error);
-}
-    	console.log("Backen is started");
+	}
+	console.log("Backen is started");
 });
