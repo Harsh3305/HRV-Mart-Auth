cbca479eb20cd9a24b5b3551225cca8aaa52f724
Harsh3305 <harshverma3305@gmail.com>
2022-12-16 13:55:54 +0530

    Minor changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const authRoute = require("./routes/auth");
 const homeRoute = require("./routes/home");
+const { json } = require("express");
 require('dotenv').config();
 
 const app = express();
 
-app.use(cors)
+app.use(cors());
+app.use(json());
 app.use("", authRoute);
 app.use("", homeRoute);
 
