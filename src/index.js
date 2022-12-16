8e2e7f458728e0ecd0f0f098b9a447af1b2e18d2
Harsh3305 <harshverma3305@gmail.com>
2022-12-16 13:45:13 +0530

    Added middleware for authentication

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoute = require("./routes/auth");
+const homeRoute = require("./routes/home");
 require('dotenv').config();
 
 const app = express();
 
 app.use("", authRoute);
+app.use("", homeRoute);
 
 app.listen(process.env.PORT, (error) => {
 	if (error) {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,87 @@
 const router = require("express").Router();
 
 router.post("/login", async (req, res) => {
-
+    const password = req.body.password;
+    const email = req.body.email;
+    if (!passwordValidator(password)) {
+        res.status(500).send(PASSWORD_INVALID)
+    }
+    else if (!emailValiator(email)) {
+        res.status(500).send(EMAIL_INVALID)
+    }
+    else {
+        const hashPassword = generateHashedPassword(password)
+        // Send email and password to backend server to login
+        const data = JSON.stringify({
+            "email": email,
+            "hashedPassword": hashPassword
+        });
+        const config = {
+            method: 'post',
+            url: `${process.env.BACKEND_URL}/auth/login`,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+        axios(config)
+            .then(function (response) {
+                const result = JSON.stringify(response.data);
+                if (result == "false") {
+                    res.status(400).send("Login Failed")
+                }
+                else {
+                    // Create JWT and send it to user
+                    const token = createJWT(email)
+                    res.status(200).json(
+                        {
+                            jwt: token,
+                        }
+                    )
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+                res.status(500).send(error.message);
+            });
+    }
 })
 router.post("/signUp", async (req, res) => {
 
 })
 
+function createJWT(userId) {
+    const token = jwt.sign({
+        userId: userId,
+        type: "user"
+    },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "10m",
+            issuer: "HRV-Mart",
+        }
+    )
+    return token
+}
+function passwordValidator(password) {
+    if (password == null || password.length < 8) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+function emailValiator(email) {
+    if (email == null || email.split("@").length != 2) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+function generateHashedPassword(password) {
+    return crypto.createHash("sha256", process.env.HASH_SECRET)
+        .update(password)
+        .digest("hex");
+}
 module.exports = router;
