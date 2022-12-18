4fd49890dc05c2f1f63c2d3a0064f14fd9e56e37
Harsh3305 <harshverma3305@gmail.com>
2022-12-18 20:22:29 +0530

    Rename hom.js to uerProfile.js

--- src/routes/user/home.js
+++
@@ -1,9 +0,0 @@
-const { isUser } = require("../../middleware/authenticate");
-
-const router = require("express").Router();
-
-router.get("", isUser, async (req, res) => {
-    res.status(200).send(req.user)
-})
-
-module.exports = router;
