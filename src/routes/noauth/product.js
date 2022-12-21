9b5fc68445ed74a003fb1c3b5bb6108e5cc6f9eb
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2022-12-21 06:04:53 +0530

    HMA-8-Implement-products-features (#11)

--- src/routes/noauth/product.js
+++ src/routes/noauth/product.js
@@ -16,9 +16,55 @@ router.get("/", (req, res) => {
         }
     );
 });
+router.get("/search", (req, res) => {
+    // console.log(req);
+    getRequest(
+        `products/search?query=${req.query.query}`,
+        {},
+        {},
+        (error, result) => {
+            if (error) {
+                res.status(error.response.statusCode).send(error.response.body);
+            }
+            else {
+                res.status(200).send(result);
+            }
+        }
+    );
+});
 router.get("/:id", (req, res) => {
     getRequest(
         `products/${req.params.id}`,
+        {},
+        {},
+        (error, result) => {
+            if (error) {
+                res.status(error.response.statusCode).send(error.response.body);
+            }
+            else {
+                res.status(200).send(result);
+            }
+        }
+    );
+});
+router.get("/categories", (req, res) => {
+    getRequest(
+        "products/categories",
+        {},
+        {},
+        (error, result) => {
+            if (error) {
+                res.status(error.response.statusCode).send(error.response.body);
+            }
+            else {
+                res.status(200).send(result);
+            }
+        }
+    );
+});
+router.get("/category/:id", (req, res) => {
+    getRequest(
+        `products${req.url}`,
         {},
         {},
         (error, result) => {
