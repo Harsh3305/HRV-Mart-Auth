f4e4a17dbe21567c9daf1117643a08210c0225b9
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2022-12-21 05:37:20 +0530

    HMA 7 add product request for user (#10)
    * Added Products router functions
    
    * Added product router in app
    
    * Implementer get single product

--- src/routes/noauth/product.js
+++ src/routes/noauth/product.js
@@ -16,7 +16,19 @@ router.get("/", (req, res) => {
         }
     );
 });
-router.get("/{id}");
-
-
+router.get("/:id", (req, res) => {
+    getRequest(
+        `products/${req.params.id}`,
+        {},
+        {},
+        (error, result) => {
+            if (error) {
+                res.status(error.response.statusCode).send(error.response.body);
+            }
+            else {
+                res.status(200).send(result);
+            }
+        }
+    );
+});
 module.exports = router;
