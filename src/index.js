23a086e611bd089f0de7c66421b6e1ef2b24005e
Harsh3305 <harshverma3305@gmail.com>
2022-12-24 09:50:36 +0530

    added routers in index .js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const userProfileRoute = require("./routes/user/userProfile");
 const productRouter = require("./routes/noauth/product");
 const userCartRoute = require("./routes/user/cart");
 const userCommentRouter = require("./routes/user/comment");
+const userLikeRouter = require("./routes/user/like");
 const noAuthCommentRouter = require("./routes/noauth/comment");
+const noAuthLikeRouter = require("./routes/noauth/like");
 require('dotenv').config();
 
 const app = express();
@@ -19,6 +21,8 @@ app.use("/user/cart", userCartRoute);
 app.use("/products", productRouter);
 app.use("/comment", userCommentRouter);
 app.use("/comment", noAuthCommentRouter);
+app.use("/like", userLikeRouter);
+app.use("/like", noAuthCommentRouter);
 app.listen(3000, (error) => {
 	if (error) {
 		console.error(error);
