798c9a9a24e4fb44c4aafc350785b2c0baba2a36
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2022-12-24 12:31:37 +0530

    HMA-13 implement order functionality (#16)
    * Added order router
    
    * Added order router in index.js

---
+++ src/routes/user/order.js
@@ -0,0 +1,20 @@
+const { getRequest } = require("../../networking/backendCall");
+const { isUser } = require("./../../middleware/authenticate");
+const router = require("express").Router();
+
+router.get("/", isUser, (req, res) => {
+    getRequest(
+        `order${req.url}`,
+        {},
+        {},
+        (error, result) => {
+            if (error) {
+                res.status(500).send(error)
+            }
+            else {
+                res.status(200).json(JSON.parse(result));
+            }
+        }
+    );
+});
+module.exports = router;
