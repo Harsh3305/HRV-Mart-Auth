670b2a868538f417462dde1c4374edb22227e03f
Harsh3305 <harshverma3305@gmail.com>
2023-01-03 13:00:44 +0530

    Added Get like section of user

--- src/routes/user/like.js
+++ src/routes/user/like.js
@@ -1,7 +1,7 @@
 const { getRequest, postRequest, deleteRequest } = require("../../networking/backendCall");
-const {isUser} = require("./../../middleware/authenticate");
+const { isUser } = require("./../../middleware/authenticate");
 const router = require("express").Router();
-    
+
 router.post("/", isUser, (req, res) => {
     const path = `like${req.url}`;
     postRequest(
@@ -10,7 +10,22 @@ router.post("/", isUser, (req, res) => {
             userId: req.user.userId,
             productId: req.body.productId
         },
-        {'Content-Type': 'application/json'},
+        { 'Content-Type': 'application/json' },
+        (error, result) => {
+            if (error) {
+                res.status(500).send(error)
+            }
+            else {
+                res.status(200).json(JSON.parse(result));
+            }
+        }
+    );
+});
+router.get("/", isUser, (req, res) => {
+    console.log(req.url);
+    getRequest(`like/allLikes/${req.user.userId}${req.url}`,
+        {},
+        {},
         (error, result) => {
             if (error) {
                 res.status(500).send(error)
@@ -36,7 +51,7 @@ router.get("/:productId", isUser, (req, res) => {
         }
     );
 });
-router.delete("/:productId", isUser, (req, res)=> {
+router.delete("/:productId", isUser, (req, res) => {
     deleteRequest(
         `like/${req.user.userId}/${req.params.productId}`,
         {},
